19ea762bf6a736fda21cf0d6e4d5dac1ecf15164
Edith Quijano <equijanoricalde@gmail.com>
2024-06-03 22:28:23 -0600

    Corrigiendo estilos css de link button

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,5 +1,5 @@
 @php
-$classes = 'px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold  text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150';
+$classes = 'px-4 my-1 py-2 bg-gray-800 border border-transparent rounded-md inline-block font-semibold  text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
