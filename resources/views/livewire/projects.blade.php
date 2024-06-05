88c2931ee26e898aea2d70c1d5a5f95b7f26a33a
Edith Quijano <equijanoricalde@gmail.com>
2024-06-05 09:56:35 -0600

    Agregando truncado de descripcion en tabla de proyectos

--- resources/views/livewire/projects.blade.php
+++ resources/views/livewire/projects.blade.php
@@ -82,7 +82,7 @@
                     <div class="py-2 px-2 max-w-sm mx-auto bg-white space-y-2 sm:py-2 sm:flex  sm:ml-0 sm:items-center sm:space-y-0 sm:space-x-6">
                         <div class="text-center space-y-2 sm:text-left">
                             <div class="px-3">
-                                {{$element->description}}
+                                {{ mb_strimwidth($element->description, 0, 100, "...") }}
                             </div>
                         </div>
                     </div>
