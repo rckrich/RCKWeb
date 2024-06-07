fc6a828b1d864cff56b81f3937979c8ab1322ba6
Edith Quijano <equijanoricalde@gmail.com>
2024-06-06 19:57:43 -0600

    Agregando nuevos usuarios a dar de alta

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,9 +14,38 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        /*
         DB::table('users')->insert([
             'name' => 'RCK Games',
             'email' => 'ricardoeglezg@rckgames.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        */
+
+        DB::table('users')->insert([
+            'name' => 'Landy Arvizu',
+            'email' => 'landy.arvizu@rckgames.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Eduardo Soberanis',
+            'email' => 'eduardo_soberanis@rckgames.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Zaira Briceño Games',
+            'email' => 'zairabriceno@rckgames.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
